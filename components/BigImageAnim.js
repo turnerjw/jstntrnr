9d297997fbe1de5edc5336596cbdab657780b1fb
Justin Turner <justinturner@Justins-MacBook-Pro.local>
2018-12-20 19:21:52 -0500

    Created component to display large images

---
+++ components/BigImageAnim.js
@@ -0,0 +1,52 @@
+import styled, { keyframes } from "styled-components";
+import VisibilitySensor from "react-visibility-sensor";
+
+const zoom = keyframes`
+    from {
+        transform: translate3d(0, 0, 10rem);
+    }
+    to {
+        transform: translate3d(0, 0, 0);
+    }
+`;
+
+const wipe = keyframes`
+    from {
+        clip-path: polygon(0 0, 0 0, 20% 100%, 0% 100%);
+    }
+    to {
+        clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
+    }
+`;
+
+const BigImage = styled.div`
+    margin: 5rem 0;
+    z-index: 1;
+    grid-column: ${props => props.columnStart || 1} /
+        ${props => props.columnEnd || -1};
+    grid-row: 1;
+    perspective: 50rem;
+    overflow: hidden;
+    clip-path: polygon(0 0, 0 0, 20% 100%, 0% 100%);
+    animation: ${props => (props.isVisible ? wipe : "")} 1s forwards ease;
+    img {
+        width: 100%;
+        animation: ${props => (props.isVisible ? zoom : "")} 1s forwards ease;
+    }
+`;
+
+const BigImageAnim = ({ src, alt, columnStart, columnEnd }) => (
+    <VisibilitySensor partialVisibility={true}>
+        {({ isVisible }) => (
+            <BigImage
+                isVisible={isVisible}
+                columnStart={columnStart}
+                columnEnd={columnEnd}
+            >
+                <img src={src} alt={alt} />
+            </BigImage>
+        )}
+    </VisibilitySensor>
+);
+
+export default BigImageAnim;
