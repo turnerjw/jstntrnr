4aac4d0e7bf01523ec06776171ddb1479b176f39
Justin Turner <justinturner@Justins-MacBook-Pro.local>
2018-12-29 20:16:48 -0500

    Modified Nav Style

--- components/NavMenu.js
+++ components/NavMenu.js
@@ -14,27 +14,13 @@ const Close = styled.div`
 `;
 
 const NavItem = styled.div`
-    margin: 1rem 0;
+    margin: 2rem 0;
     h1 {
         user-select: none;
         cursor: pointer;
     }
 `;
-/* &:nth-child(1) {
-            transition: opacity 0.4s 0.4s, transform 0.4s 0.4s;
-        }
 
-        &:nth-child(2) {
-            transition: opacity 0.4s 0.5s, transform 0.4s 0.5s;
-        }
-
-        &:nth-child(3) {
-            transition: opacity 0.4s 0.5s, transform 0.4s 0.5s;
-        }
-
-        &:nth-child(4) {
-            transition: opacity 0.4s 0.5s, transform 0.4s 0.5s;
-        } */
 const NavBox = styled.div`
     ${NavItem} {
         opacity: ${({ open }) => (open ? "1" : "0")};
@@ -52,11 +38,11 @@ const NavBox = styled.div`
     color: ${({ theme }) => theme.bg};
     transform: ${({ open }) => (open ? "translateX(0%)" : "translateX(-100%)")};
     transform-origin: left;
-    transition: transform 0.4s ease;
+    transition: transform 0.4s ease, z-index 0s 0.2s;
     ::before {
         content: "";
-        grid-column: 7 / -1;
-        grid-row: 1 / 6;
+        grid-column: 2 / -1;
+        grid-row: 1;
         height: 100%;
         transform: ${({ open }) => (open ? "scaleX(0.05)" : "scaleX(1)")};
         transform-origin: right;
@@ -101,22 +87,16 @@ class NavMenu extends Component {
                     <Close onClick={this.handleCloseMenu}>
                         <FontAwesomeIcon icon="times" />
                     </Close>
-                    <GridItem columns="4 / -1" rows="2">
+                    <GridItem columns="4 / -1" rows="1" alignSelf="center">
                         <NavItem>
                             <h1>Home</h1>
                         </NavItem>
-                    </GridItem>
-                    <GridItem columns="4 / -1" rows="3">
                         <NavItem>
                             <h1>About</h1>
                         </NavItem>
-                    </GridItem>
-                    <GridItem columns="4 / -1" rows="4">
                         <NavItem>
                             <h1>Projects</h1>
                         </NavItem>
-                    </GridItem>
-                    <GridItem columns="4 / -1" rows="5">
                         <NavItem>
                             <h1>Contact</h1>
                         </NavItem>
