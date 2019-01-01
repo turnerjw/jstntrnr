0159779aa25c140c5d07cd2cac9fa4e01504441d
Justin Turner <justinturner@Justins-MacBook-Pro.local>
2019-01-01 15:47:42 -0500

    Refactored BigImage component

--- components/BigImageAnim.js
+++ components/BigImageAnim.js
@@ -1,45 +1,22 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import VisibilitySensor from "react-visibility-sensor";
 
-const zoom = keyframes`
-    from {
-        transform: translate3d(0, 0, 10rem);
-    }
-    to {
-        transform: translate3d(0, 0, 0);
-    }
-`;
-
-const wipe = keyframes`
-    from {
-        clip-path: polygon(0 0, 0 0, 20% 100%, 0% 100%);
-    }
-    to {
-        clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
-    }
-`;
-
 const BigImage = styled.div`
-    grid-column: 1 / -1;
-    grid-row: 1;
-    z-index: 1;
     perspective: 50rem;
     overflow: hidden;
-    clip-path: polygon(0 0, 0 0, 20% 100%, 0% 100%);
-    animation: ${props => (props.isVisible ? wipe : "")} 1.2s forwards
-        cubic-bezier(0.23, 1, 0.32, 1);
+    clip-path: ${({ isVisible }) =>
+        isVisible
+            ? "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+            : "polygon(0 0, 0 0, 20% 100%, 0% 100%)"};
+    transition: clip-path 1.2s cubic-bezier(0.23, 1, 0.32, 1);
+
     img {
         width: 100%;
         min-height: 25rem;
         object-fit: cover;
-        animation: ${props => (props.isVisible ? zoom : "")} 1s forwards
-            ease-out;
-    }
-
-    @media (min-width: 1000px) {
-        grid-column: ${props => props.columnStart || 1} /
-            ${props => props.columnEnd || -1};
-        grid-row: 1;
+        transform: ${({ isVisible }) =>
+            isVisible ? "translate3d(0, 0, 0)" : "translate3d(0, 0, 10rem)"};
+        transition: transform 1s ease-out;
     }
 `;
 
